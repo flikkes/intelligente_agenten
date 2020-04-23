bb5c3e05ec717a263724c6c87100532be32348c3
flikkes <lpke.flx@gmail.com>
2020-04-23 10:09:32 +0200

    finished quick and dirty gui

--- Labyrint/Agent.py
+++ Labyrint/Agent.py
@@ -1,22 +1,26 @@
 import sys
 import Labyrinth
 import time
+import threading
 
 class Agent:
     num = 0 
     x = 0
     y = 0
     labyrinth = None
-    def __init__(self, x, y, labyrinth):
+    callback = None
+    def __init__(self, x, y, labyrinth, callback):
         self.num = time.time()*1000
         self.x = x
         self.y = y
         self.labyrinth = labyrinth
-        print(str(self.num)+': Created new agent. Exploring now...')
-        self.explore()
-        
-    
+        self.callback = callback
+        print(str(self.num)+': Created new agent. Exploring...')
+        t = threading.Thread(target=self.explore)
+        t.start()
+
     def explore(self):
+        self.callback()
         walkableSpots = []
         if (self.labyrinth.isFinish(self.x, self.y)):
             print(str(self.num)+': Agent found the exit at x: '+str(self.x)+', y: '+str(self.y))
@@ -36,12 +40,13 @@ class Agent:
             self.explore()
         if (len(walkableSpots)>1):
             for num, spot in enumerate(walkableSpots, start = 1):
-                agent = Agent(spot['x'], spot['y'], self.labyrinth)
+                print(str(self.num)+': Creating new agent')
+                agent = Agent(spot['x'], spot['y'], self.labyrinth, self.callback)
             self.x = walkableSpots[0]['x']
             self.y = walkableSpots[0]['y']
             self.explore()
         if (len(walkableSpots) == 0):
-            print(str(self.num)+': Dead end reached, dying now...')
+            print(str(self.num)+': Dead end reached, dying...')
 
         
     
--- Labyrint/app.py
+++ Labyrint/app.py
@@ -1,5 +1,36 @@
 from Agent import Agent
 from Labyrinth import Labyrinth
+import time
+import tkinter
+from tkinter import *
 
 labyrinth = Labyrinth()
-initialAgent = Agent(10, 0, labyrinth)
+
+top = tkinter.Tk()
+top.geometry("500x500")
+
+canvas = Canvas(top, height=len(labyrinth.fields)*15, width=len(labyrinth.fields[0])*15)
+canvas.pack()
+
+def callback():
+    time.sleep(1)
+    for i in range(len(labyrinth.fields)):
+        for j in range(len(labyrinth.fields[i])):
+            val = labyrinth.fields[i][j]
+            fill="#ffffff"
+            if val == 1:
+                fill = "#34ebc9"
+            if val == 2:
+                fill = "#edea39"
+            if val == 3:
+                fill = "#d92f23"
+            if val == 4:
+                fill = "#5334eb"
+            canvas.create_rectangle(j*15, i*15, j*15+8, i*15+8, fill=fill)
+    top.update()
+
+callback()
+
+initialAgent = Agent(10, 0, labyrinth, callback)
+
+top.mainloop()
