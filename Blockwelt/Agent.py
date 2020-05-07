18d6e0ca6b429563760e21300766f985c2ab6e77
flikkes <lpke.flx@gmail.com>
2020-05-07 06:29:40 +0200

    Fixed methods for blockworld, started to fix agent logic

--- Blockwelt/Agent.py
+++ Blockwelt/Agent.py
@@ -1,44 +1,29 @@
 import Blockworld
-from Blockworld import Table
+from Blockworld import World
 
 class Agent:
 
     world = None
-    goal = {}
+    goal = None
 
     def __init__(self, world, goal):
         self.world = world
         self.goal = goal
-    
-    def reachGoal(self):
-        if (self.world.finish == self.goal) or (self.world.start == self.goal):
-            return True
-        while not ((self.world.finish == self.goal) or (self.world.start == self.goal)):
-            while self.world.start and not self.checkRestOrder(self.world.start):
-                self.world.putDown(self.world.start[-1])
-            while self.world.finish and not self.checkRestOrder(self.world.finish):
-                self.world.putDown(self.world.finish[-1])
-            if self.world.start:
-                while self.world.table:
-                    elem = self.world.table[0]
-                    self.world.stack(elem, self.world.start[-1])
-                    if not self.checkRestOrder(self.world.start):
-                        self.world.putDown(elem)
-            elif self.world.finish:
-                while self.world.table:
-                    elem = self.world.table[0]
-                    self.world.stack(elem, self.world.finish[-1])
-                    if not self.checkRestOrder(self.world.finish):
-                        self.world.putDown(elem)
-            else:
-                self.world.stack(self.world.table[0], self.__getGroundElementOfGoal())
-        
-    def checkRestOrder(self, arr):
-        for i in range(len(arr)):
-            if arr[i] != self.goal[i]:
-                return False
-        return True
 
-    def __getGroundElementOfGoal(self):
-        return self.goal[0]
+    def fulfillGroundGoal(self):
+        for x in self.world.start:
+            if self.goal.onground(x):
+                if self.world.onground(x):
+                    break
+                if self.world.clear(x):
+                    if not self.world.putdown(x):
+                        self.freeGroundFor(x)
+
+
+    def freeGroundFor(self, x):
+        if not x in self.world.start:
+            return True                
+        
+
+        return True
             
--- Blockwelt/Blockworld.py
+++ Blockwelt/Blockworld.py
@@ -1,15 +1,16 @@
-class Table:
+class World:
 
     start = []
     table = []
     finish = []
+    hand = None
 
     def __init__(self, start, table, finish):
         self.start = start
         self.table = table
         self.finish = finish
 
-    def onGround(self, x):
+    def ontable(self, x):
         if self.start:
             if self.start[0] == x:
                 return True
@@ -21,24 +22,37 @@ class Table:
                 return True
         return False
     
+    def holds(self, x):
+        return self.hand == x
+    
     def stack(self, x, y):
-        if self.clear(x) and self.clear(y):
-            if self.onTable(y):
+        if self.holds(x) and self.clear(y):
+            if y in self.table:
                 self.table.append(x)
-                self.__removeFromStart(x)
-                self.__removeFromFinish(x)
+                self.hand = None
                 return True
             if y in self.finish:
                 self.finish.append(x)
-                self.__removeFromStart(x)
-                self.__removeFromTable(x)
+                self.hand = None
                 return True
             if  y in self.start:
                 self.start.append(x)
-                self.__removeFromFinish(x)
-                self.__removeFromTable(x)
+                self.hand = None
                 return True
         return False
+
+    def unstack(self, x, y):
+        if self.clear(x) and self.on(x, y) and self.handempty():
+            self.hand = x
+            self.__removeFromStart(x)
+            self.__removeFromTable(x)
+            self.__removeFromFinish(x)
+            return True
+        return False
+
+
+    def handempty(self):
+        return self.hand == None
 
     def __removeFromTable(self, x):
         if x in self.table:
@@ -69,9 +83,6 @@ class Table:
             if self.finish[-1] == x:
                 return True
         return False
-    
-    def onTable(self, x):
-        return x in self.table
 
     def on(self, x, y):
         if x in self.start and y in self.start:
@@ -87,10 +98,34 @@ class Table:
         xIndex = arr.index(x)
         return arr.index(y) == xIndex-1
 
-    def putDown(self, x):
-        if self.clear(x):
-            self.table.append(x)
-            self.__removeFromStart(x)
-            self.__removeFromFinish(x)
-            return True
+    def putdown(self, x):
+        if self.holds(x):
+            if not self.start:
+                self.start.append(x)
+                self.hand = None
+                return True
+            if not self.table:
+                self.table.append(x)
+                self.hand = None
+                return True
+            if not self.finish:
+                self.finish.append(x)
+                self.hand = None
+                return True
+        return False
+
+    def pickup(self, x):
+        if self.handempty():
+            if len(self.start) == 1 and self.start[0] == x:
+                self.hand = x
+                self.__removeFromStart(x)
+                return True
+            if len(self.table) == 1 and self.table[0] == x:
+                self.hand = x
+                self.__removeFromTable(x)
+                return True
+            if len(self.finish) == 1 and self.finish[0] == x:
+                self.hand = x
+                self.__removeFromFinish(x)
+                return True
         return False
